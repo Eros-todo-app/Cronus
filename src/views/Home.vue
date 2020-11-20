5e6dbc88d3baf4d47999ce7d8f5a71f6d470de5b
Eddie Englund <properllerbucher@gmail.com>
2020-11-20 17:53:06 +0100

    extended lorem and fix linting issue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,9 @@
     <div class="info">
       <h1>Open Source <br />Todo App</h1>
       <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua.
       </p>
       <Button route="/register" name="Sign up!" />
     </div>
@@ -17,8 +18,8 @@ import Button from "@/components/button.vue";
 
 @Component({
   components: {
-    Button,
-  },
+    Button
+  }
 })
 export default class Home extends Vue {}
 </script>
